322c5536bf02c2d7cbd28470e7bb6f2dd62d964e
Marcos Fernandes <marcosfernandes@uniplaclages.edu.br>
2022-11-06 22:37:38 -0300

    Opa

---
+++ Questions.js
@@ -0,0 +1,58 @@
+let questions = [
+  {
+  numb: 1,
+  question: "Quando é 1+1 ?",
+  answer: "2",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 2,
+  question: "Quando é 2+2 ?",
+  answer: "4",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 3,
+  question: "Quando é 3+4 ?",
+  answer: "7",
+  options: [
+    "7",
+    "13",
+    "6",
+    "9"
+  ]
+},
+  {
+    numb: 4,
+    question: "Quando é 3+3 ?",
+    answer: "6",
+    options: [
+      "7",
+      "13",
+      "6",
+      "9"
+    ]
+  },
+{
+    numb: 3,
+    question: "Quando é 3+10 ?",
+    answer: "13",
+    options: [
+      "7",
+      "13",
+      "6",
+      "9"
+    ]
+},
+
+];
